62b2b3f3aa9c0246b6341c88d760ce743459d74e
Pathum Weerathunga <pathummadushanka.weerathunga@student.kyh.se>
2021-09-13 22:11:16 +0200

    change weather upon selected value on dropdown

--- src/Weather.js
+++ src/Weather.js
@@ -6,73 +6,50 @@ import { CITY } from './cityData';
 
 export default function Weather() {
 
-    const [city, setCity] = useState('Colombo');
+    const [city, setCity] = useState('colombo');
     const [weather, setWeather] = useState('');
     const [cityList, setCityList] = useState(null);
 
+
+
     useEffect(() => {
 
-        setCityList(CITY.cities.map(i => ({
-            value: i, label: i
-        })));
-
+        if (!cityList) {
+            setCityList(CITY.cities.map(i => ({
+                value: i, label: i
+            })));
+        }
     }, [])
 
-    const searchWeather = async (e) => {
-        e.preventDefault();
-        const data = await fetchWeather(city);
+
+    const searchWeather = async (selectedCity) => {
+        const data = await fetchWeather(selectedCity);  //setState is async, .. send Selected val. direct to fetch data
         setWeather(data);
 
-        console.log(data);
-
+        console.log(data)
     }
 
     const handleChange = (e) => {
-        setCity(e.value);
-        console.log(city)
+        if (e) {
+
+            setCity(e.value);
+
+            searchWeather(e.value);
+
+        }
+
     };
 
-    /* let options;
-    if (cityList) {
-        options = CITY.cities.map(i => ({
-            value: i, label: i
-        }));
-
-    } */
-
-    /* const options = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
-    ] */
-
-
-    /* 
-        const options = test.map(v => ({
-            label: v,
-            value: v
-          })); */
 
 
     return (
         <div>
+            <Select options={cityList}
+                onChange={handleChange}
+                value={city}
+            />
 
-            <form action="" onSubmit={searchWeather}>
-
-
-                <Select options={cityList}
-                    onChange={handleChange}
-                /* value={city} */
-                />
-
-                {/* <input type="text"
-                    placeholder={"Enter city"}
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                /> */}
-                <button>Enter</button>
-            </form>
-            {weather.main && (
+            {weather && weather.main && (
                 <div>
                     <h2 className="city-name">
                         <span>{weather.name}</span>
@@ -82,6 +59,10 @@ export default function Weather() {
                         <h1>උෂ්ණත්වය
                             {Math.round(weather.main.temp)}
                             <sup>&deg;C</sup></h1>
+
+                        <h2>ඇඟට දැනෙනවා {Math.round(weather.main.feels_like)}
+                            <sup>&deg;C</sup>
+                            වගේ..</h2>
                     </div>
                     <div className="info">
                         <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
